b07f5397e942811468d0fd72ea2edf29cd3260d3
saitejamandadi99 <saitejamandadi99>
2025-09-04 16:43:30 +0530

    added multer to the routes

--- ScreenRApp-backend/routes/recordingRoutes.js
+++ ScreenRApp-backend/routes/recordingRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {uploadRecording,getAllRecording,getRecordingById,deleteRecordingsById} = require('../controllers/recordingControllers');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
-router.post('/upload',uploadRecording);
+router.post('/upload',upload.single('recording'),uploadRecording);
 router.get('/all',getAllRecording);
 router.get('/:id',getRecordingById);
 router.delete('/:id',deleteRecordingsById);
+
+module.exports = router;
